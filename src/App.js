ed37b72ad46e29202a4db26f47260d5c3ae9dd74
overwatch <ovuobasampson@gmail.com>
2019-04-16 09:17:49 -0700

    added routes and updated app js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import NavCarousel from './components/NavCarousel';
 import ItemCarousel from './components/ItemCarousel';
 import Footer from './components/Footer/Footer.js';
 import {BrowserRouter, Route} from 'react-router-dom';
+import Details from './views/Details/Details'
 
 class App extends Component {
   render() {
     return (
        <BrowserRouter>
       <div className="App">
-      <Route />
-              <NavHead/>
+
+      <Route exact path="/" render = { props => (
+        <React.Fragment>
+           <NavHead/>
            <NavCarousel/>
        <div className="container">
        <ItemCarousel />
        </div>
        <Footer />
+        </React.Fragment>
+      )}/>
+      <Route path="/details" component={Details} />   
       </div>
         </BrowserRouter>
     );
